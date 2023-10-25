ba38c8892867642713d3dc8befbfece165f43d5c
Ayub Malik <ayub.malik@gmail.com>
2023-10-25 18:55:43 +0100

    eip

--- ch4-ownership/src/main.rs
+++ ch4-ownership/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    println!("Hello, world!");
-    let x = rand::random::<u8>();
-    println!("x = {}", x);
+    scope();
+}
+
+fn scope() {
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str() appends a literal to a String
+    println!("{}", s); // this will print `hello, world!`
 }
